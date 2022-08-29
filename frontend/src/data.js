436a9d4b6317e56b3365456e6563b86a3ec2e02e
Akinade Mayowa <akinade.mayowa@gmail.com>
2022-08-30 00:49:15 +0100

    added products list

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [{
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt-1',
+            category: 'Shirts',
+            image: '/images/p1.png', // used 679px by 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Adiddas Slim Shirt',
+            slug: 'nike-slim-shirt-2',
+            category: 'Wears',
+            image: '/images/p2.jpeg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Chelsea Slim Shirt',
+            slug: 'nike-slim-shirt-3',
+            category: 'Pants',
+            image: '/images/p3.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Longe Slim Shirt',
+            slug: 'nike-slim-shirt-4',
+            category: 'Tops',
+            image: '/images/p4.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Drum Slim Shirt',
+            slug: 'nike-slim-shirt-5',
+            category: 'Pants',
+            image: '/images/p5.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+
+    ]
+}
+
+export default data;
