4d1f5fa0ec8fd7941e051eaa90bfde326fa2598e
Akinade Mayowa <akinade.mayowa@gmail.com>
2022-11-07 22:04:55 +0100

    used reducers to manage state

--- backend/data.js
+++ backend/data.js
@@ -1,5 +1,6 @@
 const data = {
     products: [{
+            _id: "1",
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt-1',
             category: 'Shirts',
@@ -12,48 +13,52 @@ const data = {
             description: 'high quality shirt'
         },
         {
+            _id: "2",
             name: 'Adiddas Slim Shirt',
             slug: 'nike-slim-shirt-2',
             category: 'Wears',
             image: '/images/p2.jpeg',
             price: 120,
-            countInStock: 10,
+            countInStock: 0,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
             description: 'high quality shirt'
         },
         {
+            _id: "3",
             name: 'Chelsea Slim Shirt',
             slug: 'nike-slim-shirt-3',
             category: 'Pants',
             image: '/images/p3.png',
             price: 120,
-            countInStock: 10,
+            countInStock: 8,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
             description: 'high quality shirt'
         },
         {
+            _id: "4",
             name: 'Longe Slim Shirt',
             slug: 'nike-slim-shirt-4',
             category: 'Tops',
             image: '/images/p4.png',
             price: 120,
-            countInStock: 10,
+            countInStock: 20,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
             description: 'high quality shirt'
         },
         {
+            _id: "5",
             name: 'Drum Slim Shirt',
             slug: 'nike-slim-shirt-5',
             category: 'Pants',
             image: '/images/p5.png',
             price: 120,
-            countInStock: 10,
+            countInStock: 0,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,24 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
+app.get('/api/product/:slug', (req, res) => {
+    const product = data.products.find((x) => x.slug === req.params.slug);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not found' })
+    }
+});
+
+app.get('/api/product/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params._id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not found' })
+    }
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
